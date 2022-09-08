28b05134a49adff3fa2e1cb2192d703ab4995dc8
ANURADHA <anuradhamachiraju888@gmail.com>
2022-09-08 21:06:47 +0530

    hakuna matata

---
+++ Biggest.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+class Biggest
+{
+  public static void main(String args[])
+  {
+   int a=Integer.parseInt(args[0]);
+   int b=Integer.parseInt(args[1]);
+   int c=Integer.parseInt(args[2]);
+   if(a>b)
+   {
+     if(a>c)
+     {
+       System.out.println(a);
+     }
+   }
+   else if(b>c)
+   {
+     System.out.println(b);
+   }
+   else
+   {
+     System.out.println(c);
+   }
+  }
+}
